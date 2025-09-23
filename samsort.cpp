ad56930a80c2d83cfb03de6fd5900f57ec62a721
0xethjayadithya_g7dev/ <144100792+jayadithya-g7@users.noreply.github.com>
2025-09-23 11:31:34 +0530

    try for q1

--- samsort.cpp
+++ samsort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <mpi.h>
+#include <mpi.h> 
 #include <random> // for better random number generation 
 
 // Merges two sorted vectors and keeps either the lower or upper half
